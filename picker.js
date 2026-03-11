883388cbfcff7fe0c7d85aaf026f2f3cde5cb016
jctannu4 <whereischason@protonmail.com>
2026-03-11 13:45:02 -0400

    checking my proficiency

---
+++ picker.js
@@ -0,0 +1,20 @@
+let randomNumber = Math.floor(Math.random() * 5);
+function getRandomItem(val) {
+    val = randomNumber;
+    switch (val) {
+        case 4:
+            return 'Panera'
+        case 3:
+            return 'Cookout'
+        case 2:
+            return 'Chickfila'
+        case 1:
+            return 'Zaxbys'
+        case 0:
+            return 'Wendys'
+        default:
+            return 'Dont eat lol';
+    }
+}
+
+console.log(getRandomItem())
