14f8720b60406460695b270b154fb6f55d456524
Koeia <whereischason@protonmail.com>
2026-03-24 13:36:51 -0400

    modified:   picker.js 	modified:   rockPaperScissors.js

--- picker.js
+++ picker.js
@@ -1,21 +1,21 @@
 //I always have a hard time picking where to eat, made this to avoid the difficulty
 
-let randomNumber = Math.floor(Math.random() * 5);
 function getRandomItem(val) {
-    val = randomNumber;
-    switch (val) {
-        case 4:
-            return 'Panera'
-        case 3:
-            return 'Cookout'
-        case 2:
-            return 'Chickfila'
-        case 1:
-            return 'Zaxbys'
-        case 0:
-            return 'Wendys'
-        default:
-            return 'Dont eat lol';
-    }
+  val = Math.floor(Math.random() * 5);
+  switch (val) {
+    case 4:
+      return "Panera";
+    case 3:
+      return "Cookout";
+    case 2:
+      return "Chickfila";
+    case 1:
+      return "Zaxbys";
+    case 0:
+      return "Wendys";
+    default:
+      return "Dont eat lol";
+  }
 }
-console.log(getRandomItem())
+console.log(getRandomItem());
+
--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,59 +1,60 @@
-const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase();
-    switch (userInput) {
-        case 'rock':
-            return 'rock';
-        case 'paper':
-            return 'paper';
-        case 'scissors':
-            return 'scissors';
-        default:
-            return 'error'
-    }
-}
+const getUserChoice = (userInput) => {
+  userInput.toLowerCase();
+  switch (userInput) {
+    case "rock":
+      return "rock";
+    case "paper":
+      return "paper";
+    case "scissors":
+      return "scissors";
+    default:
+      return "error";
+  }
+};
 
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    switch (randomNumber) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
-    }
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
 }
 
 function determineWinner(userChoice, computerChoice) {
-    if (userChoice === 'rock' && computerChoice === 'scissors') {
-        return 'Player wins';
-    } else if (userChoice === 'rock' && computerChoice === 'paper') {
-        return 'Computer wins'
-    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-        return 'Player wins';
-    } else if (userChoice === 'paper' && computerChoice === 'rock') {
-        return 'Player wins';
-    } else if (userChoice === 'paper' && computerChoice === 'scissors') {
-        return 'Computer wins';
-    } else if (userChoice === 'scissors' && computerChoice === 'rock') {
-        return 'Computer wins';
-    } else {
-        return 'tied'
-    }
+  if (userChoice === "rock" && computerChoice === "scissors") {
+    return "Player wins";
+  } else if (userChoice === "rock" && computerChoice === "paper") {
+    return "Computer wins";
+  } else if (userChoice === "scissors" && computerChoice === "paper") {
+    return "Player wins";
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+    return "Player wins";
+  } else if (userChoice === "paper" && computerChoice === "scissors") {
+    return "Computer wins";
+  } else if (userChoice === "scissors" && computerChoice === "rock") {
+    return "Computer wins";
+  } else {
+    return "tied";
+  }
 }
-
 
 //
 
 function playGame() {
-    let userChoice = getUserChoice('rock');
-    let computerChoice = getComputerChoice();
-    if (userChoice === 'Error') {
-        console.log('Please make a selection')
-    } else {
-        console.log(`player chooses ${userChoice}, computer chooses ${computerChoice}`);
-        console.log(determineWinner(userChoice, computerChoice))
-    }
+  let userChoice = getUserChoice("rock");
+  let computerChoice = getComputerChoice();
+  if (userChoice === "Error") {
+    console.log("Please make a selection");
+  } else {
+    console.log(
+      `player chooses ${userChoice}, computer chooses ${computerChoice}`,
+    );
+    console.log(determineWinner(userChoice, computerChoice));
+  }
 }
 
 playGame();
