6a536ea9eaf895d6272959af6d54b4438bc82ea0
Koeia <whereischason@protonmail.com>
2026-04-15 11:06:28 -0400

    delete comment

--- hotel.js
+++ hotel.js
@@ -1,24 +1,3 @@
-/*
-const totalCost = (hotelOne, hotelTwo) => {
-  const gasPrice = (mpg, ppg, totalMiles) => {
-    let roundTrip = totalMiles * 2;
-    let totalGallons = roundTrip / mpg;
-    let priceOfGas = totalGallons * ppg;
-    console.log(
-      `this trip will require ${Math.round(totalGallons * 100) / 100} gallons of gas and cost $${Math.round(priceOfGas * 100) / 100}`,
-    );
-  };
-  gasPrice(36, 3.71, 307);
-  let myCost = (hotelOne + hotelTwo) / 2 ;
-  let herCost = (hotelOne + hotelTwo) / 2;
-  console.log(`Sarah owes $${herCost}, Chason owes $${myCost}`);
-};
-
-totalCost(200, 150);
-*/
-
-// refactor nesting function into single function
-
 const totalCost = (hotelOne, hotelTwo, mpg, ppg, totalMiles) => {
   let roundTrip = totalMiles * 2;
   let totalGallons = roundTrip / mpg;
